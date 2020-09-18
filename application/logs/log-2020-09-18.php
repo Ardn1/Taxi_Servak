a993efdf991a6b7626d599bb2fbd5fe8f5615385
Ardn1 <49320695+Ardn1@users.noreply.github.com>
2020-09-18 19:25:07 +0300

    Первый день сервера

--- application/logs/log-2020-09-18.php
+++ application/logs/log-2020-09-18.php
@@ -21,3 +21,21 @@ ERROR - 2020-09-18 14:01:12 --> 404 Page Not Found: Docs/105062546.jpg
 ERROR - 2020-09-18 14:01:12 --> 404 Page Not Found: Docs/114983870.jpg
 ERROR - 2020-09-18 14:16:38 --> 404 Page Not Found: api/Contact/upload_auto2
 ERROR - 2020-09-18 14:16:39 --> 404 Page Not Found: api/Contact/upload_auto2
+ERROR - 2020-09-18 17:13:00 --> Severity: Notice --> Undefined variable: data C:\xampp\htdocs\crm\application\views\my\rent.php 25
+ERROR - 2020-09-18 17:13:00 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\crm\application\views\my\rent.php 25
+ERROR - 2020-09-18 17:13:00 --> Severity: Notice --> Undefined variable: data C:\xampp\htdocs\crm\application\views\my\rent.php 26
+ERROR - 2020-09-18 17:13:00 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\crm\application\views\my\rent.php 26
+ERROR - 2020-09-18 17:13:00 --> Severity: Notice --> Undefined variable: data C:\xampp\htdocs\crm\application\views\my\rent.php 27
+ERROR - 2020-09-18 17:13:00 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\crm\application\views\my\rent.php 27
+ERROR - 2020-09-18 17:13:00 --> Severity: Notice --> Undefined variable: data C:\xampp\htdocs\crm\application\views\my\rent.php 29
+ERROR - 2020-09-18 17:13:00 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\crm\application\views\my\rent.php 29
+ERROR - 2020-09-18 17:15:04 --> Severity: Notice --> Undefined variable: data C:\xampp\htdocs\crm\application\views\my\rent.php 25
+ERROR - 2020-09-18 17:15:04 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\crm\application\views\my\rent.php 25
+ERROR - 2020-09-18 17:15:33 --> Severity: Notice --> Undefined variable: data C:\xampp\htdocs\crm\application\views\my\rent.php 25
+ERROR - 2020-09-18 17:15:33 --> Severity: Notice --> Trying to get property of non-object C:\xampp\htdocs\crm\application\views\my\rent.php 25
+ERROR - 2020-09-18 18:22:08 --> Severity: Notice --> Undefined property: stdClass::$API C:\xampp\htdocs\crm\application\views\my\rent.php 140
+ERROR - 2020-09-18 18:22:08 --> Severity: Notice --> Undefined property: stdClass::$API C:\xampp\htdocs\crm\application\views\my\rent.php 142
+ERROR - 2020-09-18 18:22:08 --> Severity: Notice --> Undefined property: stdClass::$API C:\xampp\htdocs\crm\application\views\my\rent.php 140
+ERROR - 2020-09-18 18:22:08 --> Severity: Notice --> Undefined property: stdClass::$API C:\xampp\htdocs\crm\application\views\my\rent.php 142
+ERROR - 2020-09-18 18:22:08 --> Severity: Notice --> Undefined property: stdClass::$API C:\xampp\htdocs\crm\application\views\my\rent.php 140
+ERROR - 2020-09-18 18:22:08 --> Severity: Notice --> Undefined property: stdClass::$API C:\xampp\htdocs\crm\application\views\my\rent.php 142
