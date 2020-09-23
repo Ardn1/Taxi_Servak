76d237721b2b953a63a3b6e2fa07211b4a7031b9
Sergei Chamkin <sergei.chamkin@gmail.com>
2020-09-23 16:25:24 +0300

    123
    123

--- application/views/my/orders_edit.php
+++ application/views/my/orders_edit.php
@@ -3,7 +3,7 @@
         crossorigin="anonymous"></script>
 <style>
     #rotater {
-        
+
     }
 </style>
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -65,11 +65,12 @@
                 Водительское удостоверение (внешняя сторона)
             </div>
             <div class="card-body p-0">
-                <img src="<?php
+                <img  src="<?php
                 if (strpos($order->doc_vu_1, '.') !== false)
                     echo base_url('docs/' . $order->doc_vu_1);
                 else echo 'data:image/jpg;base64,'.$order->doc_vu_1
-                ?>" class="w-100" id="rotater" onclick="rotate(this)">
+                ?>" class="w-100">
+
             </div>
         </div>
 
